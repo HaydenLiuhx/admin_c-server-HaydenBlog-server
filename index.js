81206804b832a86a8809c444cf46cdd195989175
HaydenLiu <huangxunliu@gmail.com>
2020-08-20 09:58:07 +1000

    userAPI

--- index.js
+++ index.js
@@ -77,6 +77,62 @@ router.post('/api/login', async (req, res) => {
 })
 
 
+// 添加用户
+router.post('/api/manage/user/add', (req, res) => {
+  // 读取请求参数数据
+  const {username, password} = req.query
+
+  // 处理: 判断用户是否已经存在, 如果存在, 返回提示错误的信息, 如果不存在, 保存
+  // 查询(根据username)
+  UserModel.findOne({username})
+    .then(user => {
+      // 如果user有值(已存在)
+      if (user) {
+        // 返回提示错误的信息
+        res.send({status: 1, msg: '此用户已存在'})
+        return new Promise(() => {
+        })
+      } else { // 没值(不存在)
+        // 保存
+        return UserModel.create({...req.query, password: md5(password || 'admin')})
+      }
+    })
+    .then(user => {
+      // 返回包含user的json数据
+      res.send({status: 0, data: user})
+    })
+    .catch(error => {
+      console.error('注册异常', error)
+      res.send({status: 1, msg: '添加用户异常, 请重新尝试'})
+    })
+})
+
+
+// 更新用户
+router.post('/api/manage/user/update', (req, res) => {
+  const user = req.body
+  UserModel.findOneAndUpdate({_id: user._id}, user)
+    .then(oldUser => {
+      const data = Object.assign(oldUser, user)
+      // 返回
+      res.send({status: 0, data})
+    })
+    .catch(error => {
+      console.error('更新用户异常', error)
+      res.send({status: 1, msg: '更新用户异常, 请重新尝试'})
+    })
+})
+
+// 删除用户
+router.post('/api/manage/user/delete', (req, res) => {
+  const {userId} = req.body
+  UserModel.deleteOne({_id: userId})
+    .then((doc) => {
+      res.send({status: 0})
+    })
+})
+
+
 
 // router.get('/api/profile', async (req, res) => {
 //     res.send(req.user)
